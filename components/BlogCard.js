8c22462657f3ec31ff77899162e3f04d4e22b3fd
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-10-03 21:43:32 +0200

    initial commit

---
+++ components/BlogCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Link from "next/link";
+import styles from "../styles/BlogCard.module.css";
+
+const BlogCard = ({
+  title,
+  authorName,
+  authorImg,
+  coverPhoto,
+  content,
+  slug,
+  datePublished,
+}) => {
+  return (
+    <div className={styles.card}>      
+      <Link href={`/posts/${slug}`}>
+        <div className={styles.imgContainer}>
+          <img src={coverPhoto} alt={title} />
+        </div>
+      </Link>
+      <div className={styles.text}>
+        <h2>{title}</h2>
+        <div className={styles.details}>
+          {/* <div>
+            <p>{content}</p>
+          </div> */}
+          <div className={styles.author}>
+            <div className={styles.imgAuthor}>
+              <img src={authorImg} alt={authorName} />
+            </div>
+            <h3>{authorName}</h3>
+          </div>
+          <div className={styles.date}>
+            <h3>{datePublished}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
