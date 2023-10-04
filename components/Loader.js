b20a6c8eeebf53e2e30eea7a08be3cd45111f15c
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-10-04 18:11:32 +0200

    loader css

--- components/Loader.js
+++ components/Loader.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import styles from '../styles/Loader.module.css'
+import React from "react";
+import styles from "../styles/Loader.module.css";
 
 const Loader = () => {
   return (
     <div className={styles.loader}>
-      Loading...
+      <div className={styles.L}>L</div>
+      <div className={styles.outter}>
+        <div className={styles.mid}></div>
+      </div>
+      <div className={styles.ADING}>ADING</div>
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
